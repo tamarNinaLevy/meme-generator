63b7f88b36005c12e67164586d485588c6d4cb91
tamarNinaLevy <tamarlevy2002@gmail.com>
2025-01-10 13:46:47 +0200

    getCorrectPath again 2

--- js/services/editorService.js
+++ js/services/editorService.js
@@ -25,7 +25,7 @@ function getImage(id) {
 }
 
 function getCorrectedPath(imgSrc) {
-    const basePath = location.hostname.includes('github.io') ? './' : '../../';
+    const basePath = location.hostname.includes('github.io') ? '' : '../../';
     return basePath + imgSrc;
 }
 
